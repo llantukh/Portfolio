e2142a725340b65b7a2152957d0f953499592319
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-12-25 19:46:47 -0500

    got the layout template to appear

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,13 +2,15 @@ function routerConfig ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('root', {
       abstract: true,
-      templateUrl: 'templates/layout.tpl.html',
+      templateUrl: 'templates/layout.tpl.html'
     })
     .state('root.home', {
       url: '/',
-      templateUrl: 'templates/home.tpl.html',
-      controller: 'homeController as home'
+      templateUrl: 'templates/home.tpl.html'
+      // controller: 'homeController as home'
     })
+
+    $urlRouterProvider.otherwise('/');
 }
 
 routerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
