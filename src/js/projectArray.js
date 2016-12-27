8f607264437b322af0a2ecf89ec1730554e3a2fe
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-12-26 20:57:57 -0500

    added a bunch of commas

--- src/js/projectArray.js
+++ src/js/projectArray.js
@@ -2,40 +2,40 @@ const allProjects = [
 
   // IronStack
   {
-    name: 'IronStack'
-    summary: 'Helping TIY students organize and collaborate.'
-    description: 'IronStack was inspired by our student experience at The Iron Yard. During our time here we realized that it takes much more than being able to code to be a good developer; you must be able to collaborate with others and oraganize your resources. We structured the app around the curriculum at The Iron Yard, which focuses on a new language concept every week. Using IronStack, students can leave posts on the assignment they are working on and comment on any post in their class. The posts are separated into four categories, each with a different view. There is also an admin user experience which includes adding new classes and assignments. Admin users can also delete any post made my any other user.'
-    tech: 'This app was built with two teammates in about two and a half weeks. We built both the front and back end and used AngularJS, MomentJS, Bulma, jQuery, and AdonisJS.'
-    liveLink: 'http://tiy-lelawalker-ironstackfe.surge.sh/#/start'
-    ghLink: 'https://github.com/aliensfinalproject/'
+    name: 'IronStack',
+    summary: 'Helping TIY students organize and collaborate.',
+    description: 'IronStack was inspired by our student experience at The Iron Yard. During our time here we realized that it takes much more than being able to code to be a good developer; you must be able to collaborate with others and oraganize your resources. We structured the app around the curriculum at The Iron Yard, which focuses on a new language concept every week. Using IronStack, students can leave posts on the assignment they are working on and comment on any post in their class. The posts are separated into four categories, each with a different view. There is also an admin user experience which includes adding new classes and assignments. Admin users can also delete any post made my any other user.',
+    tech: 'This app was built with two teammates in about two and a half weeks. We built both the front and back end and used AngularJS, MomentJS, Bulma, jQuery, and AdonisJS.',
+    liveLink: 'http://tiy-lelawalker-ironstackfe.surge.sh/#/start',
+    ghLink: 'https://github.com/aliensfinalproject/',
     image: 'images/ironstack.png'
-  }
+  },
   // Aqua Lust
   {
-    name: 'Aqua Lust'
-    summary: 'Basically Instagram...for one user.'
-    description: 'Aqua Lust allows users to take pictures, upload them, like them, and leave comments. Just make sure your pictures are aqua flavored.'
-    tech: 'This app was built with a teammate during a weekend Hackathon. We built both the front and back end and used AngularJS, Bulma, and AdonisJS.'
-    liveLink: 'http://tiy-lelawalker-hackathon-frontend.surge.sh/#/'
-    ghLink: 'https://github.com/llantukh/Hackathon-FrontEnd'
+    name: 'Aqua Lust',
+    summary: 'Basically Instagram...for one user.',
+    description: 'Aqua Lust allows users to take pictures, upload them, like them, and leave comments. Just make sure your pictures are aqua flavored.',
+    tech: 'This app was built with a teammate during a weekend Hackathon. We built both the front and back end and used AngularJS, Bulma, and AdonisJS.',
+    liveLink: 'http://tiy-lelawalker-hackathon-frontend.surge.sh/#/',
+    ghLink: 'https://github.com/llantukh/Hackathon-FrontEnd',
     image: 'images/aquaLust.png'
-  }
+  },
   // Testify
   {
-    name: 'Testify'
-    summary: 'Gilded Rose Nightmare'
-    description: 'Horrific Legacy code refactoring. We had to work out the famous Gilded Rose inventory system and make sense of it without breaking the existing tests. And Google was forbidden during the assignment.'
-    ghLink: 'https://github.com/llantukh/testify'
-    image: 'images/gildedRose.jpg'
-  }
+    name: 'Testify',
+    summary: 'Gilded Rose Nightmare',
+    description: 'Horrific Legacy code refactoring. We had to work out the famous Gilded Rose inventory system and make sense of it without breaking the existing tests. And Google was forbidden during the assignment.',
+    ghLink: 'https://github.com/llantukh/testify',
+    image: 'images/gildedRose.jpg',
+  },
   // SoundCloud Music App
   {
-    name: 'SoundCloud Music App'
-    summary: 'Search for an artist. Listen to them.'
-    description: 'This was one of my first times digging into an API. I created an app where users can search for an arist, get back a list of songs, and listen to them. Because there could never be enough music apps that do the same thing.'
-    tech: 'jQuery'
-    liveLink: 'http://tiy-lelawalker-soundcloud-music-app.surge.sh'
-    ghLink: 'https://github.com/llantukh/soundcloud-music-app'
+    name: 'SoundCloud Music App',
+    summary: 'Search for an artist. Listen to them.',
+    description: 'This was one of my first times digging into an API. I created an app where users can search for an arist, get back a list of songs, and listen to them. Because there could never be enough music apps that do the same thing.',
+    tech: 'jQuery',
+    liveLink: 'http://tiy-lelawalker-soundcloud-music-app.surge.sh',
+    ghLink: 'https://github.com/llantukh/soundcloud-music-app',
     image: 'images/SoundCloud.png'
   }
 
