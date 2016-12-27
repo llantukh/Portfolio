48ed97eb4113c29df94b6b9e09ce9bca30eb4ade
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-12-26 20:33:03 -0500

    adding project info. halfway done.

---
+++ src/js/projectArray.js
@@ -0,0 +1,46 @@
+const allProjects = [
+
+  // IronStack
+  {
+    name: 'IronStack'
+    summary: 'Helping TIY students organize and collaborate.'
+    description: 'IronStack was inspired by our student experience at The Iron Yard. During our time here we realized that it takes much more than being able to code to be a good developer; you must be able to collaborate with others and oraganize your resources. We structured the app around the curriculum at The Iron Yard, which focuses on a new language concept every week. Using IronStack, students can leave posts on the assignment they are working on and comment on any post in their class. The posts are separated into four categories, each with a different view. There is also an admin user experience which includes adding new classes and assignments. Admin users can also delete any post made my any other user.'
+    tech: 'This app was built with two teammates in about two and a half weeks. We built both the front and back end and used AngularJS, MomentJS, Bulma, jQuery, and AdonisJS.'
+    liveLink: 'http://tiy-lelawalker-ironstackfe.surge.sh/#/start'
+    ghLink: 'https://github.com/aliensfinalproject/'
+    image: 'images/ironstack.png'
+  }
+  // Aqua Lust
+  {
+    name: 'Aqua Lust'
+    summary: 'Basically Instagram...for one user.'
+    description: 'Aqua Lust allows users to take pictures, upload them, like them, and leave comments. Just make sure they are aqua flavored.'
+    tech: 'This app was built with a teammate during a weekend Hackathon. We built both the front and back end and used AngularJS, Bulma, and AdonisJS.'
+    liveLink: 'http://tiy-lelawalker-hackathon-frontend.surge.sh/#/'
+    ghLink: 'https://github.com/llantukh/Hackathon-FrontEnd'
+    image: 'images/aquaLust.png'
+  }
+  // Testify
+  {
+    name:
+    summary:
+    description:
+    tech:
+    liveLink:
+    ghLink:
+    image:
+  }
+  // SoundCloud Music App
+  {
+    name:
+    summary:
+    description:
+    tech:
+    liveLink:
+    ghLink:
+    image:
+  }
+
+]
+
+export { allProjects };
