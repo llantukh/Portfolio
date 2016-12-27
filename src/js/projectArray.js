fb54505eb15f0012f2633d79c3e5880bce02f0a9
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-12-26 23:15:04 -0500

    messing with styling

--- src/js/projectArray.js
+++ src/js/projectArray.js
@@ -3,7 +3,7 @@ const allProjects = [
   // IronStack
   {
     name: 'IronStack',
-    summary: 'Helping TIY students organize and collaborate.',
+    summary: 'Helping TIY students stay sane.',
     description: 'IronStack was inspired by our student experience at The Iron Yard. During our time here we realized that it takes much more than being able to code to be a good developer; you must be able to collaborate with others and oraganize your resources. We structured the app around the curriculum at The Iron Yard, which focuses on a new language concept every week. Using IronStack, students can leave posts on the assignment they are working on and comment on any post in their class. The posts are separated into four categories, each with a different view. There is also an admin user experience which includes adding new classes and assignments. Admin users can also delete any post made my any other user.',
     tech: 'This app was built with two teammates in about two and a half weeks. We built both the front and back end and used AngularJS, MomentJS, Bulma, jQuery, and AdonisJS.',
     liveLink: 'http://tiy-lelawalker-ironstackfe.surge.sh/#/start',
