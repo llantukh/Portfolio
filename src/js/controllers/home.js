06f8d0d70572c4fe13d68eac6ea7fe69714f5e12
Liliya Lantukh <liliya.lantukh@gmail.com>
2017-01-25 08:55:46 -0500

    switched to single page layout, need only one controller

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,5 +1,16 @@
-function homeController () {
+function homeController (allProjects, $location, $anchorScroll) {
+
+  let vm = this;
+  vm.all = allProjects;
+
+  vm.scrollTo = scrollTo;
+
+  function scrollTo (id){
+       $location.hash(id);
+       $anchorScroll();
+    }
 
 }
 
+homeController.inject = ['allProjects', '$location', '$anchorScroll']
 export { homeController };
