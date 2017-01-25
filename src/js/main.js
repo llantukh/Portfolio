aaa29c838b1fc17d20df5136d43cd76a5492dfd1
Liliya Lantukh <liliya.lantukh@gmail.com>
2017-01-25 10:17:02 -0500

    deleted extra code that is no longer necessary since switching to single page layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,12 @@
 import angular from 'angular';
 import 'angular-ui-router';
-// import _ from 'lodash';
 
 import { routerConfig } from './routes';
 import { homeController } from "./controllers/home.js";
-// import { projectsController } from "./controllers/projects";
 import { allProjects} from './projectArray.js'
 
 angular
   .module('app', ['ui.router'])
   .config(routerConfig)
   .controller('homeController', homeController)
-  // .controller('projectsController', projectsController)
   .constant('allProjects', allProjects)
--- src/js/routes.js
+++ src/js/routes.js
@@ -8,26 +8,7 @@ function routerConfig ($stateProvider, $urlRouterProvider) {
     .state('root.home', {
       url: '/',
       templateUrl: 'templates/home.tpl.html',
-      // controller: 'homeController as home'
     })
-    // .state('root.projects', {
-    //   url: '/projects',
-    //   templateUrl: 'templates/projects.tpl.html',
-    //   controller: 'projectsController as projects'
-    // })
-    // .state('root.projects.single', {
-    //   url: '/single/:name',
-    //   templateUrl: 'templates/single.tpl.html',
-    //   controller: 'singleController as single'
-    // })
-    // .state('root.resume', {
-    //   url: '/resume',
-    //   templateUrl: 'templates/resume.tpl.html'
-    // })
-    // .state('root.contact', {
-    //   url: '/contact',
-    //   templateUrl: 'templates/contact.tpl.html'
-    // })
 
     $urlRouterProvider.otherwise('/');
 }
